13eb69f67991597ba809d98e4ed1b18cc474383f
IzePhanthakarn <izephanthakarn@hotmail.com>
2024-11-23 02:32:56 +0700

    update multilang on personal infomation

--- modules/PersonalInfo/Language.tsx
+++ modules/PersonalInfo/Language.tsx
@@ -1,16 +1,18 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const Language = () => {
+  const { t } = useTranslation();
   return (
     <Card className="mb-6">
       <CardHeader className="py-2 px-4 mb-0'">
-        <h2 className="font-semibold text-primary text-lg">Language</h2>
+        <h2 className="font-semibold text-primary text-lg">{t("personalInfo.language")}</h2>
       </CardHeader>
       <CardContent className="flex justify-between text-xs px-2 pl-8 sm:pl-6 pb-4">
         <ul className="list-disc">
-            <li className="text-base sm:text-xs xl:text-sm 2xl:text-base text-muted-foreground">Thai</li>
-            <li className="whitespace-nowrap text-base sm:text-xs xl:text-sm 2xl:text-base text-muted-foreground">English (intermediate)</li>
+            <li className="personalinfo_language-value">{t("personalInfo.language1")}</li>
+            <li className="whitespace-nowrap personalinfo_language-value">{t("personalInfo.language2")}</li>
         </ul>
       </CardContent>
     </Card>
--- modules/PersonalInfo/PersonalInfomation.tsx
+++ modules/PersonalInfo/PersonalInfomation.tsx
@@ -1,44 +1,46 @@
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { Separator } from '@/components/ui/separator';
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 const PersonalInfomation = () => {
+  const { t } = useTranslation();
   return (
     <Card className="mb-6 w-full">
       <CardHeader className='py-2 px-4 mb-2 sm:mb-0'>
-        <h2 className='font-semibold text-primary text-lg'>Personal Infomation</h2>
+        <h2 className='font-semibold text-primary text-lg'>{t("personalInfo.title")}</h2>
       </CardHeader>
       <CardContent className="flex flex-col sm:flex-row justify-between px-4 pb-4 space-y-1 sm:space-y-0">
         <div className="flex flex-col space-y-1 sm:space-y-0">
             <div className="flex">
-                <h3 className='w-full sm:w-16 md:w-24 lg:w-16 xl:w-28 text-base sm:text-xs xl:text-sm 2xl:text-base font-semibold'>Nickname:</h3>
-                <p className="ml-2 w-full sm:w-fit text-end sm:text-start text-base sm:text-xs xl:text-sm 2xl:text-base text-muted-foreground">Ize</p>
+                <h3 className='personalinfo_column1-title'>{t("personalInfo.nickname")}:</h3>
+                <p className="personalinfo_column1-value">{t("personalInfo.nicknameValue")}</p>
             </div>
             <div className="flex">
-                <h3 className='w-full sm:w-16 md:w-24 lg:w-16 xl:w-28 text-base sm:text-xs xl:text-sm 2xl:text-base font-semibold'>Age:</h3>
-                <p className="ml-2 w-full sm:w-fit text-end sm:text-start text-base sm:text-xs xl:text-sm 2xl:text-base text-muted-foreground">23 Years</p>
+                <h3 className='personalinfo_column1-title'>{t("personalInfo.age")}:</h3>
+                <p className="personalinfo_column1-value">{t("personalInfo.ageValue")}</p>
             </div>
         </div>
         <Separator orientation="vertical" className='h-auto' />
         <div className="flex flex-col space-y-1 sm:space-y-0">
             <div className="flex">
-                <h3 className='w-full sm:w-20 md:w-28 lg:w-20 xl:w-36 text-base sm:text-xs xl:text-sm 2xl:text-base font-semibold whitespace-nowrap'>Date of Birth:</h3>
-                <p className="ml-2 w-full sm:w-fit text-end sm:text-start text-base sm:text-xs xl:text-sm 2xl:text-base text-muted-foreground">17 June 2001</p>
+                <h3 className='personalinfo_column2-title whitespace-nowrap'>{t("personalInfo.dateOfBirth")}:</h3>
+                <p className="personalinfo_column2-value">{t("personalInfo.dateOfBirthValue")}</p>
             </div>
             <div className="flex">
-                <h3 className='w-full sm:w-20 md:w-28 lg:w-20 xl:w-36 text-base sm:text-xs xl:text-sm 2xl:text-base font-semibold'>Nationality:</h3>
-                <p className="ml-2 w-full sm:w-fit text-end sm:text-start text-base sm:text-xs xl:text-sm 2xl:text-base text-muted-foreground">Thai</p>
+                <h3 className='personalinfo_column2-title'>{t("personalInfo.nationality")}:</h3>
+                <p className="personalinfo_column2-value">{t("personalInfo.nationalityValue")}</p>
             </div>
         </div>
         <Separator orientation="vertical" />
         <div className="flex flex-col space-y-1 sm:space-y-0">
             <div className="flex">
-                <h3 className='w-full sm:w-12 md:w-16 lg:w-12 xl:w-20 text-base sm:text-xs xl:text-sm 2xl:text-base font-semibold'>Height:</h3>
-                <p className="ml-2 w-full sm:w-fit text-end sm:text-start text-base sm:text-xs xl:text-sm 2xl:text-base text-muted-foreground">160 cm</p>
+                <h3 className='personalinfo_column3-title'>{t("personalInfo.height")}:</h3>
+                <p className="personalinfo_column3-value">{t("personalInfo.heightValue")}</p>
             </div>
             <div className="flex">
-                <h3 className='w-full sm:w-12 md:w-16 lg:w-12 xl:w-20 text-base sm:text-xs xl:text-sm 2xl:text-base font-semibold'>Weight:</h3>
-                <p className="ml-2 w-full sm:w-fit text-end sm:text-start text-base sm:text-xs xl:text-sm 2xl:text-base text-muted-foreground">49 kg</p>
+                <h3 className='personalinfo_column3-title'>{t("personalInfo.weight")}:</h3>
+                <p className="personalinfo_column3-value">{t("personalInfo.weightValue")}</p>
             </div>
         </div>
       </CardContent>
